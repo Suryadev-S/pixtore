956953d123c59f6285cb8922a7fc7783648a49ab
Suryadev <suryadev@pop-os.localdomain>
2024-08-22 16:52:21 +0530

    added programmatic routing when there is no userId remaining to add a homepage kind of scene in it.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,21 +4,29 @@ import { useQueryPost } from "@/lib/queriesAndMutations/queries";
 import { useAuth } from "@clerk/nextjs";
 import { Skeleton } from "@/components/ui/skeleton"
 import { Post } from "@/lib/types";
-import { Key } from "react";
-import { useRouter } from "next/router";
+import { Key, useEffect } from "react";
+// import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 
 export default function Home() {
-  // const router = useRouter();
+  const router = useRouter();
   const { isLoaded, userId } = useAuth();
   const query = useQueryPost();
-  if (!isLoaded || !userId) {
-    return (
-      <div>
-        user data not loaded yet or no userId
-      </div>
-    )
-  }
+  // if (!isLoaded || !userId) {
+  //   return (
+  //     <div>
+  //       user data not loaded yet or no userId
+  //     </div>
+  //   )
+  //   router.push('/testPage');
+  // }
+
+  useEffect(() => {
+    if (!isLoaded || !userId) {
+      router.push("/testPage");
+    }
+  }, [])
 
   if (query.isLoading) {
     return (
